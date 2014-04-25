90a35734b9d1cdb7ef6e0e0208877d6f67e3adec
Robin Miller & Lauren Yeiser <student@epicodus.com>
2014-04-25 15:49:03 -0700

    makes it so players are notified when the game is finished and removes the links from the board so players cannot keep playing

--- app/controllers/make_moves_controller.rb
+++ app/controllers/make_moves_controller.rb
@@ -5,9 +5,12 @@ class MakeMovesController < ApplicationController
     @space = @game.board.spaces.find_by(:number => params[:space_number])
     @space.mark_by(@game.active_player)
 
+    @is_winner = "false"
+
     if @game.win?
-      flash[:notice] = "You Win"
+      @is_winner = "true"
     end
+
     @game.change_turns
     respond_to do |format|
       format.html { redirect_to :back }
